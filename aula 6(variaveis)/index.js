a681c0e961f5cfbe3ef0793338712ab209884a08
Cesar Romero <cesarromero34@gmail.com>
2025-06-19 20:22:06 -0300

    conceitos básicos sobre variáveis

---
+++ aula 6(variaveis)/index.js
@@ -0,0 +1,12 @@
+// Var é mais antigo, não recomendado o uso
+// Let é mais recente, recomendado o uso
+// Const é mais recente e usado quando é uma constante, 
+// que não vai ter seu valor alterado ao longo do programa
+
+let nome = 'João da Silva'
+
+console.log(`${nome} nasceu em 1984`)
+console.log(`Em 2000 ${nome} conheceu Maria`)
+console.log(`${nome} casou-se com Maria em 2012`)
+console.log(`Maria teve um filho com ${nome} em 2015`)
+console.log(`O filho de ${nome} se chama Eduardo`)
