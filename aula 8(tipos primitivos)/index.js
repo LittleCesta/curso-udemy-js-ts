3a4d6a5a2f8b631eb7e2c1525fe1099c17f86c30
Cesar Romero <cesarromero34@gmail.com>
2025-06-19 20:22:48 -0300

    conceitos básicos sobre tipos primitivos

---
+++ aula 8(tipos primitivos)/index.js
@@ -0,0 +1,17 @@
+const nome = 'Menine'// String -> Texto
+const nome1 = "Menine"// String -> Texto
+const nome2 = `Menine`// String -> Texto
+
+const num = 10;// Number -> Numero
+const num2 = 10.52;// Number -> Numero
+
+let nomeAluno; // undefined -> não aponta pra local nenhum na memória. No caso de undefined, é quando não definimos nada e deixamos o js colocar o valor como undefined
+let sobrenomeAluno = null // null -> não aponta pra local nenhum na memória. No caso de definirmos, definimos para null quando queremos explicitamente que não aponte para nada
+
+const aprovado = true // Boolean -> True ou False
+
+console.log(typeof nome)
+console.log(typeof num)
+console.log(typeof nomeAluno)
+console.log(typeof sobrenomeAluno)
+console.log(typeof aprovado)
