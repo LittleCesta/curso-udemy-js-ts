4720a992994b3c075c1db796834634d9b0b863ef
Cesar Romero <cesarromero34@gmail.com>
2025-06-19 20:20:55 -0300

    conceitos de console.log

---
+++ aula 1(console.log)/consolelog.js
@@ -0,0 +1,5 @@
+console.log('Cesar Romero'); //String
+console.log('Cesar "Romero"'); // String com aspas duplas
+console.log(`'Cesar' "Romero"`); // String com aspas simples
+
+console.log(35, 1.40, 20, 'Cesar'); // Numero reais, inteiros e string no mesmo console log(Separado por virgula)
