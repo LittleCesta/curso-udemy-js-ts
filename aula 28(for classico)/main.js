618f8fce43ae5b22a9ee256fb5b6cbdbfdd81252
Cesar Romero <cesarromero34@gmail.com>
2025-08-02 20:47:53 -0300

    uso do 'for' classico

---
+++ aula 28(for classico)/main.js
@@ -0,0 +1,10 @@
+const frutas = ['Maçã', 'Pêra', 'Uva']
+
+for (let i = 0; i < frutas.length; i++) {
+  console.log(`Fruta: ${frutas[i]}`);
+}
+
+// for (i = 500; i >= 400; i -= 10){
+//   const par = i % 2 === 0 ? 'Par' : 'Ímpar'
+//   console.log(`Linha: ${i}, Par: ${par}`)
+// }
