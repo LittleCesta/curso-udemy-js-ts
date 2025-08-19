b85aab22014acc0e561358727ddbf546d7a03c49
Cesar Romero <cesarromero34@gmail.com>
2025-08-19 00:00:05 -0300

    feat: exercicio de logica de programação

---
+++ aula 34(exercício de logica de programação 1)/main.js
@@ -0,0 +1,13 @@
+function retornarMaior(a, b){
+  return a > b ? a : b
+}
+
+const retornarMaior2 = (a, b) => a > b ? a : b
+
+console.log(retornarMaior(10,5))
+console.log(retornarMaior(5,8))
+console.log(retornarMaior(10,69))
+console.log(retornarMaior2(10,5))
+console.log(retornarMaior2(5,8))
+console.log(retornarMaior2(10,69))
+
